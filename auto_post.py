fb757438844803b81ff35e45f87f9ee6fec2c681
rorhkdcns <119862610+rorhkdcns@users.noreply.github.com>
2026-05-24 18:49:53 +0900

    Update auto_post.py

--- auto_post.py
+++ auto_post.py
@@ -59,10 +59,14 @@ GITHUB_IMAGE_BASE_URL = "https://raw.githubusercontent.com/rorhkdcns/blogger-aut
 # 🔗 [캡쳐화면 반영] stock 폴더에 들어있는 실제 파일명과 확장자(jpg/png) 리스트입니다.
 # 이 안에서 프로그램이 매번 중복 없이 3장을 무작위로 뽑아 소제목 상단에 배치합니다.
 github_images_pool = [
-    "1.jpg", "2.png", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg",
+   "1.jpg", "2.png", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg",
     "11.jpg", "12.jpg", "13.jpg", "14.png", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg",
     "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.png", "27.jpg", "28.jpg", "29.jpg", "30.jpg",
-    "31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", "36.jpg", "37.jpg", "38.png", "39.png", "40.png"
+    "31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", "36.jpg", "37.jpg", "38.png", "39.png", "40.png",
+    "41.jpg", "42.jpg", "43.jpg", "44.jpg", "45.jpg", "46.jpg", "47.jpg", "48.jpg", "49.jpg", "50.jpg",
+    "51.jpg", "52.jpg", "53.jpg", "54.jpg", "55.jpg", "56.jpg", "57.jpg", "58.jpg", "59.jpg", "60.jpg",
+    "61.jpg", "62.jpg", "63.jpg", "64.jpg", "65.jpg", "66.jpg", "67.jpg", "68.jpg", "69.jpg", "70.jpg",
+    "71.jpg", "72.jpg"
 ]
 
 # 🔗 직접 올리신 주식 계산기 4종의 블로그스팟 주소
